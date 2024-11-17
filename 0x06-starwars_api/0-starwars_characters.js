d9eafc87541dddb19db1b76f3cbcb627d2b3606f
kamel0x <125790101+kamel0x@users.noreply.github.com>
2024-11-18 00:16:22 +0200

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,41 +1,17 @@
 #!/usr/bin/node
+
 const request = require('request');
 
-const id = process.argv[2];
-
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
-
-function getReq (url) {
-  const res = new Promise(function (resolve, reject) {
-    request(url, function (error, response, body) {
-      if (error) {
-        // pass
-      }
-
-      if (response.statusCode === 200) {
-        const person = JSON.parse(body);
-        resolve(person.name);
-      } else {
-        reject(response);
-      }
-    });
-  });
-  return res;
-}
-
-request(url, function (error, response, body) {
-  if (error) {
-    // pass
-  }
-  const film = JSON.parse(body);
-  const characters = film.characters;
-
-  const promises = characters.map(character => getReq(character));
-  Promise.all(promises)
-    .then(results => {
-      results.forEach(result => console.log(result));
-    })
-    .catch(error => {
-      console.log(error);
-    });
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
 });
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
+  });
+};
